45480dde79eca84bbe60cc64a03a3dc64db210c3
JoshuaRosado <joshua06rosado@gmail.com>
2024-10-01 09:40:27 -0500

    Animations update

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -7,61 +7,54 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @State private var dragAmount = CGSize.zero // current location
 
+// Creating OUR OWN custom view modifier to be able to use it how many times we want
+
+struct CornerRotateModifier: ViewModifier{ // view modifier
+    let amount: Double
+    let anchor: UnitPoint
     
-    let letters = Array("Hello SwiftUI")
-    @State private var enabled = false
-    @State private var dragAmount2 = CGSize.zero
-    
+    func body(content: Content) -> some View{ // function
+        content
+        .rotationEffect(.degrees(amount), anchor:anchor)// rotation with custom .degree and anchor
+        .clipped()// clipped to maintain it inside the object
+    }
+}
+
+extension AnyTransition{ // extension of the func to be use
+    static var pivot: AnyTransition{ // named pivot
+        // modifier with constant inserted
+        // starting at -90 degrees anchored at top left side
+        .modifier(active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+                  identity: CornerRotateModifier(amount: 0, anchor: .topTrailing))
+        // ending at 0 degrees anchored at top right side
+        //
+    }
+}
+
+struct ContentView: View {
+    @State private var isShowingRed = false
     
     var body: some View {
-        Section{
-
-            LinearGradient(colors: [.yellow, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
-                .frame(width: 300, height: 200)
-                .clipShape(.rect(cornerRadius: 10))
-                .offset(dragAmount) // which lets us adjust the X and Y coordinate of a view without moving other views around it.
-                .gesture( // inside of gesture
-                    DragGesture() // Built in dragGesture func
-                        .onChanged{ dragAmount = $0.translation} // can be move any where
-                        .onEnded{_ in
-                            // ADDING animation when it's released ONLY
-                            withAnimation(.bouncy) {
-                                dragAmount = .zero} // when released it will go back to the default location
-                        }
-                )
-            // ADDING animation to the whole body
-            //            .animation(.spring(duration:0.4, bounce: 0.6), value: dragAmount)
-        }
-        Section{
-            HStack(spacing: 0) {
-                ForEach( 0..<letters.count, id: \.self) { // For each letter from 0 up to letters total count
-                    num in // one number coming in
-                    Text(String(letters[num])) // display a string of the array[index]
-                        .padding(5)
-                        .font(.title)
-                        .background(enabled ? .blue: .red)
-                        .offset(dragAmount2)
-                    // ADDING animation, linear with a delay that goes +1 / 20 every time it loops
-                        .animation(.linear.delay(Double(num) / 20),value: dragAmount2)
-                    
-                }
+        ZStack {
+            Rectangle() // create rectangle shape
+                .fill(.blue) // filled in with blue color
+                .frame(width: 200, height: 200) // size
+            
+            if isShowingRed { // if Bool is true
+                Rectangle() // create a rectangle shape
+                .fill(.red) // filled with red color
+                .frame(width: 200, height: 200) // size
+                .transition(.pivot) // adding transition of ".pivot"
+                // ".pivot" = Modifier that we created
             }
-            .gesture(
-                DragGesture()
-                    .onChanged { dragAmount2 = $0.translation}
-                    .onEnded{ _ in
-                        dragAmount2 = .zero
-                        enabled.toggle()
-                    }
-            )
-            
-            
-            
-            
         }
+        .onTapGesture { // add a gesture when we tap the button
+            withAnimation { // with animation included when
+                isShowingRed.toggle() // we toggle bool (false to true and true to false
+            }
+        }
+        
     }
 }
 
